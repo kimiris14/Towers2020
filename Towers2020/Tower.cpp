4d7e953506985cbbe5c9369640a2d9ff929bb9ba
pangurus <pangurus@msu.edu>
2020-10-15 20:26:18 -0400

    Created Tower base class

--- Towers2020/Tower.cpp
+++ Towers2020/Tower.cpp
@@ -6,3 +6,13 @@
 
 #include "pch.h"
 #include "Tower.h"
+
+void CTower::Draw(CGame* game, Gdiplus::Graphics* graphics)
+{
+
+}
+
+void CTower::SetLocation(double x, double y)
+{
+
+}
--- Towers2020/Tower.h
+++ Towers2020/Tower.h
@@ -7,17 +7,52 @@
  */
 
 #pragma once
+#include "Item.h"
 
 
 
 /**
  * A base class for all towers
  */
-class CTower
+class CTower : public CItem
 {
 public:
+	
+
+	/**
+	* Constructor
+	* \param level A pointer to the level object that this item belongs to
+	* \param game A pointer to the game object that this item belongs to
+	* \param imageID The image ID for this object
+	*/
+	CTower(CLevel* level, CGame* game, int imageID) :
+		CItem(level, game, imageID) {}
+
+	/// Default constructor (disabled)
+	CTower() = delete;
+
+	///  Copy constructor (disabled)
+	CTower(const CTower&) = delete;
+
+	/// destructor 
+	~CTower() {}
+
+	/// Draw the item
+	/// \param game A pointer to the game object that this item belongs to
+	/// \param graphics The GDI+ graphics context to draw on
+	virtual void Draw(CGame* game, Gdiplus::Graphics* graphics) override;
+
+	/// Set the item location
+	/// \param x X location
+	/// \param y Y location
+	virtual void SetLocation(double x, double y) override;
+
+	/// Accept a visitor
+	/// \param visitor The visitor we accept 
+	virtual void Accept(CItemVisitor* visitor) override { }
 
 private:
 
-	int mTimeTilFire = 0;  ///< The time until the next fire of projectiles
+	/// The time until the next fire of projectiles
+	int mTimeTilFire = 0;  
 };
