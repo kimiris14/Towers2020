796d6a41a6d453a61c5ba382e6e38ac16669a40b
Dave Yonkers <yonkers4@msu.edu>
2020-10-17 16:15:40 -0400

    Create visitor to find a tile

---
+++ Towers2020/ItemVisitorFindTile.cpp
@@ -0,0 +1,30 @@
+/**
+ * \file ItemVisitorFindTile.cpp
+ *
+ * \author PaulaRed
+ */
+
+#include "pch.h"
+#include "ItemVisitorFindTile.h"
+#include "ItemTileRoad.h"
+
+
+ /** Visit a CItemTileRoad object
+ * \param road Road we are visiting */
+void CItemVisitorFindTile::VisitRoad(CItemTileRoad* road)
+{
+	if (road->GetGridX() == mTargetX && road->GetGridY() == mTargetY)
+	{
+		mTile = road;
+	}
+}
+
+/** Visit a CItemTileRoad object
+* \param road Road we are visiting */
+void CItemVisitorFindTile::VisitTile(CItemTile* tile)
+{
+	if (tile->GetGridX() == mTargetX && tile->GetGridY() == mTargetY)
+	{
+		mTile = tile;
+	}
+}
---
+++ Towers2020/ItemVisitorFindTile.h
@@ -0,0 +1,56 @@
+/**
+ * \file ItemVisitorFindTile.h
+ *
+ * \author PaulaRed
+ *
+ * Item visitor to find a road tile at a given index
+ */
+
+
+#pragma once
+
+#include "ItemVisitor.h"
+
+ // Forward references to all item types
+class CItemBalloon;
+class CItemTile;
+class CItemTileRoad;
+
+
+/** Tile visitor class to find a any given tile */
+class CItemVisitorFindTile : public CItemVisitor
+{
+public:
+
+    CItemVisitorFindTile() = delete;
+
+    /** Constructor
+     * \param x The target X value to find
+     * \param y The target Y value to find
+     */
+    CItemVisitorFindTile(int x, int y) : mTargetX(x), mTargetY(y) {}
+
+    virtual ~CItemVisitorFindTile() {}
+
+
+    virtual void VisitRoad(CItemTileRoad* road);
+    virtual void VisitTile(CItemTile* tile);
+
+
+    /** Getter for the  tile that was found (if at all)
+    * \returns A pointer to the found tile */
+    CItemTile* GetTile() { return mTile; }
+
+private:
+
+    /// The target X index we are looking for
+    const int mTargetX;
+
+    /// The target Y index we are looking for
+    const int mTargetY;
+
+    /// If we find a tile at the given index, this pointer will update
+    /// to point to the road tile that was found
+    CItemTile* mTile = nullptr;
+
+};
